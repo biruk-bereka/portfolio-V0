bcf7c3b8a6807250e19ea59a37d5f1be0e706c17
biruk-bereka <birukbeletech1212@gmail.com>
2022-12-19 20:56:07 +0300

    Add logic for open menu function

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const hamburgerButton = document.querySelector('.menu');
 const closeButton = document.querySelector('.close-button');
 const navItems = document.querySelector('.nav-items');
 const myName = document.querySelector('.my-name');
-
-hamburgerButton.addEventListener('click', openMenu);
-closeButton.addEventListener('click', closeMenu);
+const mailButton = document.getElementById('mail');
 
 function openMenu() {
-
+  hamburgerButton.classList.add('invisible');
+  myName.classList.add('invisible');
+  closeButton.classList.add('align-close');
+  navItems.classList.add('display-nav-items');
+  mailButton.classList.add('invisible');
 }
 
 function closeMenu() {
 
 }
+
+hamburgerButton.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
