3c4d655025dab5f65d645ab53348ed2f4d014e38
jmonto55 <jmonto55@gmail.com>
2022-12-19 12:14:22 -0500

    Add JavaScript file with initial set up

---
+++ index.js
@@ -0,0 +1,15 @@
+const hamburgerButton = document.querySelector('.menu');
+const closeButton = document.querySelector('.close-button');
+const navItems = document.querySelector('.nav-items');
+const myName = document.querySelector('.my-name');
+
+hamburgerButton.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
+
+function openMenu() {
+
+}
+
+function closeMenu() {
+
+}
