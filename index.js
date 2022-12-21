450c01d107337b1e1cda85e1a8a938b8a5f7254a
biruk-bereka <birukbeletech1212@gmail.com>
2022-12-21 11:49:35 +0300

    Details popup window added for mobile

--- index.js
+++ index.js
@@ -33,15 +33,23 @@ const projectData = [
     name: 'Multi-Post Stories',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    featuredImage: 'images/project-image-mobile.png',
     featuredImageMobile: 'images/project-image-mobile.png',
     featuredImageDesktop: 'images/project-image-desktop@0.75.png',
     technologies: ['css', 'html', 'Bootstrap', 'Ruby'],
     links: [
       {
-        liveVersionLink: '',
-        sourceLink: '',
+        link: 'https://biruk-bereka.github.io/portfolio-V0/',
+        linkText: 'See Live',
+        linkIcon: 'images/Icon-Export.png',
+      },
+      {
+        link: 'https://github.com/biruk-bereka/portfolio-V0',
+        linkText: 'See Source',
+        linkIcon: 'images/Vector.png',
       },
     ],
+    closeIcon: 'images/cancel-icon.png',
   },
   {
     name: 'Profesional Art Printing Data',
@@ -51,11 +59,18 @@ const projectData = [
     technologies: ['html', 'Bootstrap', 'Ruby'],
     links: [
       {
-        liveVersionLink: '',
-        sourceLink: '',
+        link: 'https://biruk-bereka.github.io/portfolio-V0/',
+        linkText: 'See Live',
+        linkIcon: 'images/Icon-Export.png',
+      },
+      {
+        link: 'https://github.com/biruk-bereka/portfolio-V0',
+        linkText: 'See Source',
+        linkIcon: 'images/Vector.png',
       },
     ],
     projectCardClass: ['card-1'],
+    closeIcon: 'images/cancel-icon.png',
   },
   {
     name: 'Data Dashboard Healthcare',
@@ -65,11 +80,18 @@ const projectData = [
     technologies: ['html', 'Bootstrap', 'Ruby'],
     links: [
       {
-        liveVersionLink: '',
-        sourceLink: '',
+        link: 'https://biruk-bereka.github.io/portfolio-V0/',
+        linkText: 'See Live',
+        linkIcon: 'images/Icon-Export.png',
+      },
+      {
+        link: 'https://github.com/biruk-bereka/portfolio-V0',
+        linkText: 'See Source',
+        linkIcon: 'images/Vector.png',
       },
     ],
     projectCardClass: ['card-2'],
+    closeIcon: 'images/cancel-icon.png',
   },
   {
     name: 'Website Protfolio',
@@ -79,11 +101,18 @@ const projectData = [
     technologies: ['html', 'Bootstrap', 'Ruby'],
     links: [
       {
-        liveVersionLink: '',
-        sourceLink: '',
+        link: 'https://biruk-bereka.github.io/portfolio-V0/',
+        linkText: 'See Live',
+        linkIcon: 'images/Icon-Export.png',
+      },
+      {
+        link: 'https://github.com/biruk-bereka/portfolio-V0',
+        linkText: 'See Source',
+        linkIcon: 'images/Vector.png',
       },
     ],
     projectCardClass: ['card-3'],
+    closeIcon: 'images/cancel-icon.png',
   },
   {
     name: 'Profesional Art Printing Data',
@@ -93,11 +122,18 @@ const projectData = [
     technologies: ['html', 'Bootstrap', 'Ruby'],
     links: [
       {
-        liveVersionLink: '',
-        sourceLink: '',
+        link: 'https://biruk-bereka.github.io/portfolio-V0/',
+        linkText: 'See Live',
+        linkIcon: 'images/Icon-Export.png',
+      },
+      {
+        link: 'https://github.com/biruk-bereka/portfolio-V0',
+        linkText: 'See Source',
+        linkIcon: 'images/Vector.png',
       },
     ],
     projectCardClass: ['card-4'],
+    closeIcon: 'images/cancel-icon.png',
   },
   {
     name: 'Data Dashboard Healthcare',
@@ -107,11 +143,18 @@ const projectData = [
     technologies: ['html', 'Bootstrap', 'Ruby'],
     links: [
       {
-        liveVersionLink: '',
-        sourceLink: '',
+        link: 'https://biruk-bereka.github.io/portfolio-V0/',
+        linkText: 'See Live',
+        linkIcon: 'images/Icon-Export.png',
+      },
+      {
+        link: 'https://github.com/biruk-bereka/portfolio-V0',
+        linkText: 'See Source',
+        linkIcon: 'images/Vector.png',
       },
     ],
     projectCardClass: ['card-2', 'card-5'],
+    closeIcon: 'images/cancel-icon.png',
   },
   {
     name: 'Website Protfolio',
@@ -121,11 +164,18 @@ const projectData = [
     technologies: ['html', 'Bootstrap', 'Ruby'],
     links: [
       {
-        liveVersionLink: '',
-        sourceLink: '',
+        link: 'https://biruk-bereka.github.io/portfolio-V0/',
+        linkText: 'See Live',
+        linkIcon: 'images/Icon-Export.png',
+      },
+      {
+        link: 'https://github.com/biruk-bereka/portfolio-V0',
+        linkText: 'See Source',
+        linkIcon: 'images/Vector.png',
       },
     ],
     projectCardClass: ['card-3', 'card-6'],
+    closeIcon: 'images/cancel-icon.png',
   },
 ];
 
@@ -138,7 +188,7 @@ const border = document.createElement('div');
 border.className = 'border-right';
 
 workTitle.append(title, border);
-
+// Project Cards
 const projectsContainer = document.createElement('div');
 const projectCards = document.createElement('div');
 projectCards.className = 'works-card';
@@ -216,3 +266,92 @@ for (let i = 0; i < projectData.length; i += 1) {
 }
 projectsContainer.append(projectCards);
 worksSection.append(workTitle, projectsContainer);
+
+// End of Project card creation
+
+// Popup window
+
+const buttons = document.querySelectorAll('button');
+
+function displayProject(m) {
+  const popupWindow = document.createElement('section');
+  document.body.appendChild(popupWindow);
+
+  const modal = document.createElement('div');
+  modal.className = 'modal';
+
+  const header = document.createElement('div');
+  header.className = 'title';
+  const name = document.createElement('h1');
+  name.innerText = projectData[m].name;
+  name.className = 'name';
+  const closeIcon = document.createElement('img');
+  closeIcon.className = 'cancel-icon';
+  closeIcon.src = projectData[m].closeIcon;
+  header.append(name, closeIcon);
+
+  const technologies = document.createElement('ul');
+  technologies.className = 'technologies';
+  const technologiesList = projectData[m].technologies;
+  for (let j = 0; j < technologiesList.length; j += 1) {
+    const list = document.createElement('li');
+    list.innerText = technologiesList[j];
+    list.className = 'list';
+    technologies.append(list);
+  }
+
+  const wrapper = document.createElement('div');
+  wrapper.className = 'wrapper';
+
+  const imageWrapper = document.createElement('div');
+  const featuredImage = document.createElement('img');
+  featuredImage.className = 'popup-image';
+  featuredImage.src = projectData[m].featuredImage;
+  imageWrapper.appendChild(featuredImage);
+
+  const contentWrapper = document.createElement('div');
+  contentWrapper.className = 'content';
+
+  const description = document.createElement('p');
+  description.innerText = projectData[m].description;
+  description.className = 'description';
+  
+  const linksWrapper = document.createElement('div');
+  linksWrapper.className = 'links';
+
+  const { links } = projectData[m];
+  for (let i = 0; i < links.length; i += 1) {
+    const link = document.createElement('a');
+    link.href = links[i].link;
+    link.innerText = links[i].linkText;
+    link.target = '_blank';
+    const linkIcon = document.createElement('img');
+    linkIcon.className = 'link';
+    linkIcon.src = links[i].linkIcon;
+
+    link.append(linkIcon);
+    linksWrapper.append(link);
+  }
+
+  contentWrapper.append(description, linksWrapper);
+  wrapper.append(imageWrapper, contentWrapper);
+  
+  modal.append(header, technologies, wrapper);
+  popupWindow.append(modal);
+
+  const main = document.querySelector('.main');
+  main.classList.toggle('blur');
+
+  popupWindow.classList.toggle('popup');
+
+  closeIcon.addEventListener('click', () => {
+    popupWindow.classList.toggle('popup');
+    main.classList.toggle('blur');
+  });
+}
+
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', () => {
+    displayProject(i);
+  });
+}
